e0bf500246960e472b7575241858591d0fce8767
Joel Zúñiga Jiménez <joelz2000@live.com>
2019-04-12 20:44:10 -0600

    Update Socket_Web_Server_NodeMCU.ino

--- Socket_Web_Server_NodeMCU/Socket_Web_Server_NodeMCU.ino
+++ Socket_Web_Server_NodeMCU/Socket_Web_Server_NodeMCU.ino
@@ -1,95 +1,113 @@
+/*
+ * WebSocketClientSocketIO.ino
+ *
+ *  Created on: 06.06.2016
+ *
+ */
+
+#include <Arduino.h>
+
 #include <ESP8266WiFi.h>
-#include <WebSocketClient.h>
+#include <ESP8266WiFiMulti.h>
 
-const char* ssid     = "Jimenez R";
-const char* password = "aves2015";
-char path[] = "/app.js";
-char host[] = "192.168.0.10";
-  
-WebSocketClient webSocketClient;
+#include <WebSocketsClient.h>
 
-// Use WiFiClient class to create TCP connections
-WiFiClient client;
+#include <Hash.h>
 
-void setup() {
-  Serial.begin(115200);
-  delay(10);
+ESP8266WiFiMulti WiFiMulti;
+WebSocketsClient webSocket;
 
-  // We start by connecting to a WiFi network
 
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  
-  WiFi.begin(ssid, password);
-  
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
+#define USE_SERIAL Serial1
 
-  Serial.println("");
-  Serial.println("WiFi connected");  
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+#define MESSAGE_INTERVAL 30000
+#define HEARTBEAT_INTERVAL 25000
 
-  delay(5000);
-  
+uint64_t messageTimestamp = 0;
+uint64_t heartbeatTimestamp = 0;
+bool isConnected = false;
 
-  // Connect to the websocket server
-  if (client.connect("192.168.0.10", 3000)) {
-    Serial.println("Connected");
-  } else {
-    Serial.println("Connection failed.");
-    while(1) {
-      // Hang on failure
+void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
+
+
+    switch(type) {
+        case WStype_DISCONNECTED:
+            USE_SERIAL.printf("[WSc] Disconnected!\n");
+            isConnected = false;
+            break;
+        case WStype_CONNECTED:
+            {
+                USE_SERIAL.printf("[WSc] Connected to url: %s\n",  payload);
+                isConnected = true;
+
+          // send message to server when Connected
+                // socket.io upgrade confirmation message (required)
+        webSocket.sendTXT("5");
+            }
+            break;
+        case WStype_TEXT:
+            USE_SERIAL.printf("[WSc] get text: %s\n", payload);
+
+      // send message to server
+      // webSocket.sendTXT("message here");
+            break;
+        case WStype_BIN:
+            USE_SERIAL.printf("[WSc] get binary length: %u\n", length);
+            hexdump(payload, length);
+
+            // send data to server
+            // webSocket.sendBIN(payload, length);
+            break;
     }
-  }
-
-  
-
-  // Handshake with the server
-  webSocketClient.path = path;
-  webSocketClient.host = host;
-  if (webSocketClient.handshake(client)) {
-    Serial.println("Handshake successful");
-  } else {
-    Serial.println("Handshake failed.");
-    while(1) {
-      // Hang on failure
-    }  
-  }
 
 }
 
+void setup() {
+    // USE_SERIAL.begin(921600);
+    USE_SERIAL.begin(115200);
+
+    //Serial.setDebugOutput(true);
+    USE_SERIAL.setDebugOutput(true);
+
+    USE_SERIAL.println();
+    USE_SERIAL.println();
+    USE_SERIAL.println();
+
+      for(uint8_t t = 4; t > 0; t--) {
+          USE_SERIAL.printf("[SETUP] BOOT WAIT %d...\n", t);
+          USE_SERIAL.flush();
+          delay(1000);
+      }
+
+    WiFiMulti.addAP("Wifi", "password");
+
+    //WiFi.disconnect();
+    while(WiFiMulti.run() != WL_CONNECTED) {
+        delay(100);
+    }
+
+    webSocket.beginSocketIO("192.168.0.10", 3000);
+    //webSocket.setAuthorization("user", "Password"); // HTTP Basic Authorization
+    webSocket.onEvent(webSocketEvent);
+
+}
 
 void loop() {
-  String data;
+    webSocket.loop();
 
-  if (client.connected()) {
-    
-    webSocketClient.getData(data);
-    if (data.length() > 0) {
-      Serial.print("Received data: ");
-      Serial.println(data);
+    if(isConnected) {
+
+        uint64_t now = millis();
+
+        if(now - messageTimestamp > MESSAGE_INTERVAL) {
+            messageTimestamp = now;
+            // example socket.io message with type "messageType" and JSON payload
+            webSocket.sendTXT("42[\"messageType\",{\"greeting\":\"hello\"}]");
+        }
+        if((now - heartbeatTimestamp) > HEARTBEAT_INTERVAL) {
+            heartbeatTimestamp = now;
+            // socket.io heartbeat message
+            webSocket.sendTXT("2");
+        }
     }
-    
-    // capture the value of analog 1, send it along
-    /*
-    pinMode(1, INPUT);
-    data = String(analogRead(1));
-    
-    webSocketClient.sendData(data);*/
-    
-  } else {
-    Serial.println("Client disconnected.");
-    while (1) {
-      // Hang on disconnect.
-    }
-  }
-  
-  // wait to fully let the client disconnect
-  delay(3000);
-  
 }
